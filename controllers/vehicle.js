4cf5e1923d8c19fbc868bffcff375e80991b9f99
Smit Gajera <smitgajeraig@gmail.com>
2021-10-16 13:37:04 +0530

    Final Controller Demo

--- controllers/vehicle.js
+++ controllers/vehicle.js
@@ -24,7 +24,7 @@ exports.postEncrypt = async (req, res, next) => {
   }
 };
 
-exports.postDecrypt = (req, res, next) => {
+exports.postDecrypt = async (req, res, next) => {
   try {
     const key = process.env.ENCRYPTION_KEY;
     // console.log(key);
@@ -46,3 +46,57 @@ exports.postDecrypt = (req, res, next) => {
     res.status(500).json(error);
   }
 };
+
+exports.dataControl = async (req, res, next) => {
+  try {
+    const key = process.env.ENCRYPTION_KEY;
+    // console.log(key);
+
+    let iv = Buffer.from(req.body.iv, "hex");
+    // console.log(iv);
+
+    const encryptedData = req.body.encryptedData;
+    let encryptedText = Buffer.from(encryptedData, "hex");
+    // console.log(encryptedData);
+
+    let decipher = crypto.createDecipheriv("aes-256-cbc", Buffer.from(key), iv);
+    let decrypted = decipher.update(encryptedText);
+    const decryptedData = decrypted.toString();
+
+    // Find This in Database
+    if (decryptedData) {
+      const data = await Vehicle.findOne({ decryptedData: vehicleRegNo }); //Vehicle Model Name in db where vehicleRegNo is same as decrypted data
+    } else {
+      // Puppeteer Code
+      // Get data from puppteer Code
+      // save this data in Database
+    }
+
+    // Now get Data from Database
+
+    // Encryption Code
+    // const key = process.env.ENCRYPTION_KEY;
+    const ivv = crypto.randomBytes(16);
+    const data = data; //Data Get from data base
+    //Encrypting text
+    function encrypt(data) {
+      let cipher = crypto.createCipheriv("aes-256-cbc", Buffer.from(key), ivv);
+      let encrypted = cipher.update(data);
+      encrypted = Buffer.concat([encrypted, cipher.final()]);
+      return {
+        iv: ivv.toString("hex"),
+        encryptedData: encrypted.toString("hex"),
+      };
+    }
+
+    // Text send to encrypt function
+    var encryptData = encrypt(data);
+    res.status(201).send(encryptData);
+
+    //
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+
--- routes/vehicle.js
+++ routes/vehicle.js
@@ -1,10 +1,15 @@
 const express = require("express");
-
 const router = express.Router();
 
+// Controllers Calling
 const vehicleControllers = require("../controllers/vehicle");
 
+// Encrypt Route
 router.post("/encrypt", vehicleControllers.postEncrypt);
+// Decrypt Route
 router.post("/decrypt", vehicleControllers.postDecrypt);
+
+// Final Route
+router.post("/", vehicleControllers.postDecrypt);
 
 module.exports = router;
